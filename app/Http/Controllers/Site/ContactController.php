666840ebec6a06521ed21857c3cb093e9be83937
bertolucci-alan <beterrababir23@gmail.com>
2021-07-20 14:20:32 -0300

    formulario de contato

--- app/Http/Controllers/Site/ContactController.php
+++ app/Http/Controllers/Site/ContactController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
+use App\Notifications\NewContact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class ContactController extends Controller
 {
@@ -19,6 +22,18 @@ class ContactController extends Controller
 
     public function form(Request $request)
     {
-        ddd($request->all());
+        //salvando no banco de dados 
+        $contact = Contact::create($request->all());
+
+
+        //passando uma collection para o envio de email
+        Notification::route('mail', config('mail.from.address'))->notify(new NewContact($contact));
+
+        // Notification::route('mail', 'taylor@example.com')
+        //     ->route('nexmo', '5555555555')
+        //     ->route('slack', 'https://hooks.slack.com/services/...')
+        //     ->notify(new InvoicePaid($invoice));
+
+        ddd($contact);
     }
 }
---
+++ app/Notifications/NewContact.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Contact;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewContact extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $contact;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Você recebeu um novo contato do site:')
+            ->line($this->contact->email)
+            ->line($this->contact->name)
+            ->line($this->contact->message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
